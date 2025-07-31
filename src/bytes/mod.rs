97bd0bb5651ab1c7c089d9ea40e8d1cb9c0130bb
ariaandika <andikaakun17@gmail.com>
2025-07-31 19:18:35 +0700

    bytes.rs module inception

--- src/bytes/mod.rs
+++ src/bytes/mod.rs
@@ -1,14 +1,15 @@
 //! Raw bytes utilities.
 mod shared;
 
-mod bytes_ref;
+#[allow(clippy::module_inception)]
+mod bytes;
 mod bytes_mut;
 mod cursor;
 mod range;
 
 use shared::{Shared, Data, DataMut};
 
-pub use bytes_ref::Bytes;
+pub use bytes::Bytes;
 pub use bytes_mut::BytesMut;
 pub use cursor::Cursor;
 pub use range::{range_of, slice_of, slice_of_bytes, slice_of_bytes_mut};
