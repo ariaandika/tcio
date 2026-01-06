679732ac2d8438b1a077f684ed2b66ef2e50e4e7
ariaandika <andikaakun17@gmail.com>
2026-01-06 20:02:12 +0700

    add Async{Read,Write} trait

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,7 +1,11 @@
 //! Provide utilities for working with asynchronous IO.
+mod read;
+mod write;
 mod io_read;
 mod io_write;
 
+pub use read::AsyncRead;
+pub use write::AsyncWrite;
 pub use io_read::{AsyncIoRead, poll_read_fn};
 pub use io_write::AsyncIoWrite;
 
