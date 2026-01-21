8f7c712a075143114c2ae546a093302993fdcc68
ariaandika <andikaakun17@gmail.com>
2026-01-21 16:05:36 +0700

    remove const and unsafe in ato{u,i}

--- src/num/num.rs
+++ src/num/num.rs
@@ -1,114 +1,54 @@
 const U64_MAX_BUF: &[u8; 20] = b"18446744073709551615";
+const I64_MAX_BUF: &[u8; 20] = b"+9223372036854775807";
 const U64_MAX_CH: usize = U64_MAX_BUF.len();
-const U64_MAX_B_PTR: *const u8 = U64_MAX_BUF.as_ptr();
+const I64_MAX_CH: usize = I64_MAX_BUF.len();
 
-const U_MAX_MOCK: &[u8; U64_MAX_CH] = b"99999999999999999999";
-const U_MAX_PTR: *const u8 = U_MAX_MOCK.as_ptr();
+#[cfg(test)]
+const I64_MIN_BUF: &[u8; 20] = b"-9223372036854775808";
 
-/// Parse ascii to unsigned integer.
-pub const fn atou(text: &[u8]) -> Option<u64> {
-    if text.len() > U64_MAX_CH || text.is_empty() {
+/// Parse ASCII to unsigned integer.
+///
+/// Returns `None` if bytes is empty or its length more than maximum possible digit.
+#[inline]
+pub fn atou(bytes: &[u8]) -> Option<u64> {
+    if (bytes.len() > U64_MAX_CH) | bytes.is_empty() {
         return None;
     }
-
-    let ptr = text.as_ptr();
-    let len = text.len();
-    let max_ptr = if len == U64_MAX_CH { U64_MAX_B_PTR } else { U_MAX_PTR };
-
-    let mut o = 0u64;
-    let mut i = 0;
-
-    while i < len {
-        unsafe {
-            // SAFETY: i < text.len()
-            let n = *ptr.add(i);
-
-            // SAFETY: i < U64_MAX_CH
-            if n < b'0' || n > *max_ptr.add(i) {
-                return None;
-            }
-
-            // SAFETY: `text.len()` is less than digit count of u64::MAX,
-            // thus it cannot overflow
-            o = o
-                .unchecked_mul(10)
-                .unchecked_add(u8::unchecked_sub(n, b'0') as u64);
-
-            // SAFETY: i < text.len()
-            i = i.unchecked_add(1);
-        }
-    }
-
-    Some(o)
+    bytes
+        .iter()
+        .try_fold(0u64, |acc, next| match next.wrapping_sub(b'0') {
+            b @ 0..=9 => acc.checked_mul(10).and_then(|n| n.checked_add(b as u64)),
+            _ => None,
+        })
 }
 
-const I64_MAX_BUF: &[u8; 19] = b"9223372036854775807";
-const I64_MAX_CH: usize = I64_MAX_BUF.len();
-const I64_MAX_B_PTR: *const u8 = I64_MAX_BUF.as_ptr();
-
-const I64_MIN_BUF: &[u8] = b"-9223372036854775808";
-
-/// Parse ascii to signed integer.
-pub const fn atoi(text: &[u8]) -> Option<i64> {
-    // this optimistic check will prevent diverge for the check below because the last digit for
-    // i64::MIN is higher than i64::MAX
-    if let I64_MIN_BUF = text {
-        return Some(i64::MIN);
-    }
-
-    let (text, sign) = match text.first() {
-        Some(&b @ (b'+' | b'-')) => (
-            // SAFETY: text length is at least 1,
-            // thus `text.len().unchecked_sub(1)` returns at least 0
-            unsafe {
-                std::slice::from_raw_parts(text.as_ptr().add(1), text.len().unchecked_sub(1))
-            },
-            if b == b'+' { 1 } else { -1 },
-        ),
-        Some(_) => (text, 1),
-        None => return None,
-    };
-
-    if text.len() > I64_MAX_CH || text.is_empty() {
+/// Parse ASCII to signed integer.
+///
+/// Returns `None` if bytes is empty, contains non-ASCII, non-sign prefix or resulting integer
+/// overflowed.
+pub fn atoi(bytes: &[u8]) -> Option<i64> {
+    if bytes.len() > I64_MAX_CH {
         return None;
     }
-
-    let ptr = text.as_ptr();
-    let len = text.len();
-    let max_ptr = if len == I64_MAX_CH { I64_MAX_B_PTR } else { U_MAX_PTR };
-
-    let mut o = 0i64;
-    let mut i = 0;
-
-    while i < len {
-        unsafe {
-            // SAFETY: i < text.len()
-            let n = *ptr.add(i);
-
-            // SAFETY: i < I64_MAX_CH
-            if n < b'0' || n > *max_ptr.add(i) {
-                return None;
-            }
-
-            // SAFETY: `text.len()` is less than digit count of i64::MAX, thus it cannot overflow
-            // SAFETY `unchecked_sub`: n < b'0'
-            o = o
-                .unchecked_mul(10)
-                .unchecked_add(u8::unchecked_sub(n, b'0') as i64);
-
-            // SAFETY: i < text.len()
-            i = i.unchecked_add(1);
-        }
-    }
-
-    // SAFETY: `sign` is either 1 or -1
-    Some(unsafe { o.unchecked_mul(sign) })
+    let (sign, bytes) = match bytes.split_first() {
+        Some((b'+' | b'-', [])) | None => return None,
+        // b'+' => -(-1)
+        // b'-' => -(1)
+        Some(sign @ (b'+' | b'-', rest)) => (-(*sign.0 as i8).wrapping_sub(0x2C), rest),
+        Some(_) => (1, bytes),
+    };
+    bytes
+        .iter()
+        .try_fold(0i64, |acc, next| match next.wrapping_sub(b'0') as i8 {
+            b @ 0..=9 => acc
+                .checked_mul(10)
+                .and_then(|n| n.checked_add((b * sign) as i64)),
+            _ => None,
+        })
 }
 
 #[test]
-fn test_atoi() {
-    // atou
-
+fn test_atou() {
     assert_eq!(Some(0), atou(b"0"));
     assert_eq!(Some(1), atou(b"1"));
     assert_eq!(Some(255), atou(b"255"));
@@ -123,21 +63,22 @@ fn test_atoi() {
     assert!(atou(b"184467440737095516155").is_none());
 
     // atou_max
-
     assert_eq!(Some(u64::MAX), atou(U64_MAX_BUF));
     assert_eq!(Some(15), atou(b"00000000000000000015"));
+    assert_eq!(Some(6), atou(b"00000000000000000006"));
 
     assert!(atou(&[0; U64_MAX_CH]).is_none());
     assert!(atou(&[255; U64_MAX_CH]).is_none());
-    assert!(atou(b"00000000000000000006").is_none());
     assert!(atou(b"18446744073709551616").is_none());
     assert!(atou(b"28446744073709551615").is_none());
     assert!(atou(b"19446744073709551615").is_none());
     assert!(atou(b"18546744073709551615").is_none());
     assert!(atou(b"99999999999999999999").is_none());
 
-    // atoi
+}
 
+#[test]
+fn test_atoi() {
     assert_eq!(Some(0), atoi(b"0"));
     assert_eq!(Some(0), atoi(b"+0"));
     assert_eq!(Some(0), atoi(b"-0"));
@@ -150,6 +91,7 @@ fn test_atoi() {
     assert_eq!(Some(1024), atoi(b"1024"));
     assert_eq!(Some(1024), atoi(b"+1024"));
     assert_eq!(Some(-1024), atoi(b"-1024"));
+    assert_eq!(Some(8), atoi(b"0000000000000000008"));
     assert_eq!(Some(999999999999999999), atoi(b"999999999999999999"));
     assert_eq!(Some(999999999999999999), atoi(b"+999999999999999999"));
     assert_eq!(Some(-999999999999999999), atoi(b"-999999999999999999"));
@@ -163,7 +105,6 @@ fn test_atoi() {
     assert!(atoi(b"92233720368547758077").is_none());
 
     // atoi_max
-
     assert_eq!(Some(i64::MAX), atoi(I64_MAX_BUF));
     assert_eq!(Some(i64::MIN), atoi(I64_MIN_BUF));
     assert_eq!(Some(-9223372036854775807), atoi(b"-9223372036854775807"));
@@ -171,7 +112,6 @@ fn test_atoi() {
 
     assert!(atoi(&[0; 20]).is_none());
     assert!(atoi(&[255; 20]).is_none());
-    assert!(atoi(b"0000000000000000008").is_none());
     assert!(atoi(b"9223372036854775808").is_none());
     assert!(atoi(b"9323372036854775807").is_none());
     assert!(atoi(b"9233372036854775807").is_none());
